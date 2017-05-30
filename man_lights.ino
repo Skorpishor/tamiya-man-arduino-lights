2f8415c7180c5f954eaebac29ac01d9720ac7c8e
Skorpishor <marius.sfara@gmail.com>
2017-05-30 21:44:02 +0200

    Beams are added

--- man_lights.ino
+++ man_lights.ino
@@ -17,6 +17,12 @@ boolean LEFT_SET =  false; // This variable mark if we RC signal to right is all
 boolean LEFT_ON = false;  // The blinker to left is off
 boolean RIGHT_ON = false; // The blinker to right is off
 
+boolean HIGH_BEAM_ON  = false;      
+boolean HIGH_BEAM_SET = false; 
+boolean LOW_BEAM_ON  = false;      
+boolean LOW_BEAM_SET = false; 
+
+
 
 // Input Chanels from RC receiver
 const int STEERING = 2;  
@@ -47,6 +53,7 @@ static int ledState = LOW;             // ledState used to set the LED
 //Change the next values to values fiting for your RC Sender/receiver. Read them from the serial prints by defining the SERIAL_DEBUG
 #define RC_LOW 1300 // the value of a chanel from where we consider that the RC sender stick is moved to links resp down
 #define RC_HIGH 1500 // the value of a chanel from where we consider that the RC sender stick is moved to right resp up
+#define RC_OFFSET 100 // OFFSET for the RC
 
 #define SERIAL_DEBUG // comment/uncomment this define if you need/do not need the serial debug, for example to read the values from the RC chanell inputs
 
@@ -86,9 +93,10 @@ void loop() {
   */
   
   blinker()   ; 
+  beams() ;
   ch_STEERING = pulseIn (STEERING,HIGH);  //Read and store channel 1
   ch_THROTTLE = pulseIn (THROTTLE,HIGH);
-  ch_AUX_TR = pulseIn (AUX_TR,HIGH);
+  
 
   if (RIGHT_ON == true) blink(BLINK_RIGHT,1000);
   if (LEFT_ON == true) blink(BLINK_LEFT,1000);  
@@ -110,6 +118,8 @@ void loop() {
   debugPrintln("--");
 */
 
+/*
+// For the Forward on RC we do nothing (at least now)
   if (ch_THROTTLE > RC_HIGH) 
   {
     debugPrintln ("WHITE LIGHT");
@@ -118,7 +128,9 @@ void loop() {
   else
   {
     digitalWrite(HIGH_BEAM,LOW);
-  }   
+  }
+*/
+     
   // If we drive back we turn the white back LED on
   if (ch_THROTTLE < RC_LOW) 
   {
@@ -226,16 +238,81 @@ void blinker()
       
     }
   }
-/*
-  void debugPrint()
+
+void beams()
+{ 
+  ch_AUX_TR = pulseIn (AUX_TR,HIGH);
+/*    
+HIGH_BEAM,OUTPUT
+LOW_BEAM,OUTPUT
+PARK_LIGHT  
+*/
+  if (ch_AUX_TR < RC_LOW - RC_OFFSET) //RC AUX_TR is to front
   {
     
-  }
-  void debugPrinLn() 
-  {
+    LOW_BEAM_ON = false;
+    if( HIGH_BEAM_SET == false )
+    {
+      if(HIGH_BEAM_ON == false)
+      {
+
+      digitalWrite(HIGH_BEAM,HIGH);
+      digitalWrite(LOW_BEAM,HIGH);
+      digitalWrite(PARK_LIGHT,HIGH);
+      
+        HIGH_BEAM_ON = true;
+      }
+      else
+      {
+        digitalWrite(HIGH_BEAM,LOW);
+        digitalWrite(LOW_BEAM,LOW);
+        digitalWrite(PARK_LIGHT,LOW);
+        
+        HIGH_BEAM_ON  = false;
+      }
+       HIGH_BEAM_SET = true; 
+    }
+    debugPrintln ("HIGH BEAM ");
 
   }
-*/
+  
+  else // RC AUX_TR is on Neutral
+  {
+    HIGH_BEAM_SET = false;
+    
+  }
+
+  
+    if (ch_AUX_TR > RC_HIGH + RC_OFFSET) //RC AUX_ST is on RIGHT
+    {
+
+      HIGH_BEAM_ON = false;
+      if( LOW_BEAM_SET == false )
+      {
+        if(LOW_BEAM_ON == false)
+        {
+          digitalWrite(LOW_BEAM,HIGH);
+          digitalWrite(PARK_LIGHT,HIGH);
+          LOW_BEAM_ON = true;
+        }
+        else
+        {
+          digitalWrite(LOW_BEAM,LOW);
+          digitalWrite(PARK_LIGHT,LOW);
+          digitalWrite(HIGH_BEAM,LOW);
+          LOW_BEAM_ON = false;
+        }
+         LOW_BEAM_SET = true; 
+      }
+      debugPrintln ("LOW BEAM ");
+    }
+    else // RC AUX_TR is on Neutral
+    {
+      LOW_BEAM_SET = false;
+      
+    }
+  }  
+
   void doNothing(int)
   {
     //do Nothing
